51c3bbeff8847b59a1053fb044dc6510df679687
plungerman <plungerman@gmail.com>
2018-06-28 09:01:27 -0500

    minimal template infrastructure for views that appear in an iframe

--- djpersonnel/settings/base.py
+++ djpersonnel/settings/base.py
@@ -61,6 +61,8 @@ DATABASES = {
     },
 }
 INSTALLED_APPS = [
+    'bootstrap_admin',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -191,6 +193,12 @@ EMAIL_FAIL_SILENTLY = False
 DEFAULT_FROM_EMAIL = ''
 SERVER_EMAIL = ''
 SERVER_MAIL=''
+# bootstrap admin
+BOOTSTRAP_ADMIN_SIDEBAR_MENU = True
+# bootstrap forms
+BOOTSTRAP4 = {
+    'required_css_class': 'required',
+}
 # logging
 LOG_FILEPATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'logs/')
 LOG_FILENAME = LOG_FILEPATH + 'debug.log'
