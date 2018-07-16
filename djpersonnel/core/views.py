41fe5d3dddc74f3c8bd913a3260ff1d7361bdb7f
plungerman <plungerman@gmail.com>
2018-07-16 15:15:18 -0500

    start dashboard development

--- djpersonnel/core/views.py
+++ djpersonnel/core/views.py
@@ -1,6 +1,21 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.conf import settings
+from django.contrib import messages
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 
-# Create your views here.
+from djzbar.decorators.auth import portal_auth_required
+
+
+
+def home(request):
+    '''
+    dashboard home page view
+    '''
+
+    return render(
+        request, 'home.html', {}
+    )
