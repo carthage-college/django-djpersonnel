7ae916bc090bcc755c8432dca115f7f7960952cb
plungerman <plungerman@gmail.com>
2018-07-16 15:15:43 -0500

    removed old code

--- djpersonnel/transaction/tests/test_views.py
+++ djpersonnel/transaction/tests/test_views.py
@@ -1,17 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.urls import resolve
 from django.conf import settings
 from django.test import TestCase
-from django.http import HttpRequest
 from django.core.urlresolvers import reverse
 
-from djpersonnel.transaction.views import form_home
-
 from djtools.utils.logging import seperator
-from djzbar.utils.informix import get_session
-from djzbar.settings import INFORMIX_EARL_TEST as EARL
 
 
 class TransactionOperationTestCase(TestCase):
@@ -23,8 +17,7 @@ class TransactionOperationTestCase(TestCase):
         print("\n")
         print("test form view")
         seperator()
-        #request = HttpRequest()
-        #response = form_home(request)
+
         response = self.client.get(reverse('transaction_form'))
         self.assertTemplateUsed(response, 'transaction/form.html')
         html = response.content.decode('utf8')
