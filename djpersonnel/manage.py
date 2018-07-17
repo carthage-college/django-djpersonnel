5a567b8ebd176f20edce83c98810745a8036f8b6
plungerman <plungerman@gmail.com>
2018-07-17 16:09:52 -0500

    replace djskeletor with djpersonnel in settings class path

--- djpersonnel/manage.py
+++ djpersonnel/manage.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djskeletor.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE', 'djpersonnel.settings.staging'
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
