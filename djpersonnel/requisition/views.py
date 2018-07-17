6ecc3bd09afa682456c66c1dfffb20bf95e60ad2
ssmolik <ssmolik@carthage.edu>
2018-07-17 10:35:13 -0500

    integrating the changes I made to the project

---
+++ djpersonnel/requisition/views.py
@@ -0,0 +1,37 @@
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render
+
+from djpersonnel.requisition.forms import OperationForm
+
+
+def form_home(request):
+    if settings.DEBUG:
+        TO_LIST = [settings.SERVER_EMAIL,]
+    else:
+        TO_LIST = [settings.PAF_EMAIL_LIST,]
+    BCC = settings.MANAGERS
+
+    if request.method=='POST':
+        form = OperationForm(request.POST)
+        if form.is_valid():
+            data = form.save()
+            email = settings.DEFAULT_FROM_EMAIL
+            if data.email:
+                email = data.email
+            subject = "[Submit] {} {}".format(
+                data.user.first_name, data.user.last_name
+            )
+            send_mail(
+                request,TO_LIST, subject, email,'transaction/email.html', data, BCC
+            )
+            return HttpResponseRedirect(
+                reverse_lazy('transaction_success')
+            )
+    else:
+        form = OperationForm()
+
+    return render(
+        request, 'requisition/form.html', {'form': form,}
+    )
