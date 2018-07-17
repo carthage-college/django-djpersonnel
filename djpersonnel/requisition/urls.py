62a2261cfd4e76141661170fee409cef82662985
ssmolik <ssmolik@carthage.edu>
2018-07-17 10:34:40 -0500

    integrating the changes I made to the project

---
+++ djpersonnel/requisition/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.views.generic import TemplateView
+
+from djpersonnel.requisition import views
+
+
+urlpatterns = [
+    # requisition form
+    url(
+        r'^$',
+        views.form_home, name='requisition_form_home'
+    ),
+]
