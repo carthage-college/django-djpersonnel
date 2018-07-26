c4d72458a55b971fcd514c8b49a46b7b5dab7dcc
plungerman <plungerman@gmail.com>
2018-07-26 12:45:25 -0500

    added radio button fields to see how django renders them and then how bootstrap4 template tag renders them; clean method for replacement_name

--- djpersonnel/requisition/forms.py
+++ djpersonnel/requisition/forms.py
@@ -4,10 +4,36 @@ from django import forms
 
 from djpersonnel.requisition.models import Operation
 
+from djtools.fields import BINARY_CHOICES
+
+
 class OperationForm(forms.ModelForm):
+
+    applicant_system = forms.ChoiceField(
+        label="""
+            Would you like any others to have access to the
+            applications in the Applicant Pro system?
+        """,
+        choices=BINARY_CHOICES, widget=forms.RadioSelect()
+    )
+    new_position = forms.ChoiceField(
+        label="Is this a new position?",
+        choices=BINARY_CHOICES, widget=forms.RadioSelect()
+    )
 
     class Meta:
         model = Operation
         # either fields or exclude is required
-        fields = '__all__'
+        exclude = [
+            'created_by','updated_by','created_at','updated_at',
+            'level1,level1_date','level2,level2_date','level3,level3_date',
+            'decline','email_approved'
+        ]
 
+    def clean_replacement_name(self):
+
+        if self.cleaned_data.get('new_position') == "No" \
+          and not self.cleaned_data.get('replacement_name'):
+            raise forms.ValidationError("You must provide a replacement name.")
+
+        return self.cleaned_data['replacement_name']
