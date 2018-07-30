e7fcc02b4c6d43f24c1322d438f4601eb04eae57
ssmolik <ssmolik@carthage.edu>
2018-07-30 15:31:10 -0500

    updated Bugested Position

--- djpersonnel/requisition/models.py
+++ djpersonnel/requisition/models.py
@@ -85,7 +85,7 @@ class Operation(models.Model):
     budgeted_position = models.CharField(
         "Is this a budgeted position?",
         max_length=4,
-        choices=BINARY_CHOICES
+        choices=BINARY_CHOICES,
     )
     salary_type = models.CharField(
         "This position is",
