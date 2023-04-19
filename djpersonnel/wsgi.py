597b26a5719a55694c218c8d72afff4270ae55b5
plungerman <plungerman@gmail.com>
2023-04-19 14:56:59 -0500

    renamed wsgi_default.py to wsgi.py since we no longer need a default

--- djpersonnel/wsgi.py
+++ djpersonnel/wsgi.py
@@ -12,17 +12,8 @@ from django.core.wsgi import get_wsgi_application
 sys.path.append('/d2/python_venv/3.8/djpersonnel/lib/python3.8/')
 sys.path.append('/d2/python_venv/3.8/djpersonnel/lib/python3.8/site-packages/')
 # django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djpersonnel.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djpersonnel.settings.shell')
 os.environ.setdefault('PYTHON_EGG_CACHE', '/var/cache/python/.python-eggs')
 os.environ.setdefault('TZ', 'America/Chicago')
-# informix
-os.environ['INFORMIXSERVER'] = ''
-os.environ['DBSERVERNAME'] = ''
-os.environ['INFORMIXDIR'] = ''
-os.environ['ODBCINI'] = ''
-os.environ['ONCONFIG'] = ''
-os.environ['INFORMIXSQLHOSTS'] = ''
-os.environ['LD_LIBRARY_PATH'] = ''
-os.environ['LD_RUN_PATH'] = os.environ['LD_LIBRARY_PATH']
 # wsgi
 application = get_wsgi_application()
