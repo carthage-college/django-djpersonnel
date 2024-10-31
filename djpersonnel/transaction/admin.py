f96411385d531589af20ff5ec6a07243ccf05736
plungerman <plungerman@gmail.com>
2024-10-31 11:13:34 -0500

    added more admin features for the PAF data model

--- djpersonnel/transaction/admin.py
+++ djpersonnel/transaction/admin.py
@@ -8,6 +8,9 @@ class TransactionOperationAdmin(admin.ModelAdmin):
     """Admin model for the PAF data model."""
 
     raw_id_fields = ('created_by', 'updated_by', 'level3_approver')
-
+    list_display = ('last_name', 'first_name', 'created_by')
+    list_max_show_all = 500
+    list_per_page = 500
+    search_fields = ['last_name']
 
 admin.site.register(Operation, TransactionOperationAdmin)
